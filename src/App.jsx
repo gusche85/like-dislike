49f66afda0a9d5fa4c4323f27995003d527538cc
gusche85 <149746619+gusche85@users.noreply.github.com>
2024-02-29 13:30:31 +0000

    Add progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { Button, ProgressBar } from 'react-bootstrap';
 export default function App() {
   const [likeCount, setLikeCount] = useState(0)
   const [dislikeCount, setDislikeCount] = useState(0)
+  const total = likeCount + dislikeCount;
+  const likePercentage = (likeCount/total)*100 || 0;
+  const dislikePercentage = (dislikeCount/total)*100 || 0;
 
 function increaseLike() {
   setLikeCount(likeCount + 1)
@@ -14,7 +17,6 @@ function dislike() {
   setDislikeCount(dislikeCount + 1)
 }
 
-  
 return (
   <>
     <div className="text-center">
@@ -23,8 +25,8 @@ return (
     </div>
     <div>
       <ProgressBar>
-        <ProgressBar variant="success" now={likeCount} key={1} />
-        <ProgressBar variant="danger" now={dislikeCount} key={2} />
+        <ProgressBar variant="success" now={likePercentage} key={1} />
+        <ProgressBar variant="danger" now={dislikePercentage} key={2} />
       </ProgressBar>
     </div>
   </>
